ebe14202895b31417e34d339ffcaf264e888b927
Andrew Dillon <andrew.dillon.j@gmail.com>
2020-10-17 18:38:38 -0500

    Setup CORS

--- src/main/java/com/qdivision/ingredientapi/controller/IngredientController.java
+++ src/main/java/com/qdivision/ingredientapi/controller/IngredientController.java
@@ -2,12 +2,10 @@ package com.qdivision.ingredientapi.controller;
 
 import com.qdivision.ingredientapi.entity.IngredientEntity;
 import com.qdivision.ingredientapi.repository.IngredientRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4003")
 public class IngredientController {
 
   private final IngredientRepository ingredientRepository;
